675b0e55cc31c7f3efd4929dbe6bd65644ac0195
Bora Akca <boraakca3535@gmail.com>
2026-09-21 15:46:38 +0000

    fixed lab2_all

--- src/lab2_3.c
+++ src/lab2_3.c
@@ -12,17 +12,32 @@
 */
 
 int is_prime(int n) {
-    // TODO: check if n is prime using loop up to sqrt(n)
-    return 0; // placeholder
+  // TODO: check if n is prime using loop up to sqrt(n)
+  for (int j = 2; j * j <= n; j++) {
+    if (n % j == 0) {
+      return 0;
+    }
+  }
+  return 1;
+
+  // placeholder
 }
-
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter an integer n (>= 2): ");
+  printf("Enter an integer n (>= 2): ");
+  scanf("%d", &n);
+  while (n < 2) {
+    printf("No prime number detected.\n Enter a higher number: ");
     scanf("%d", &n);
+  }
+  for (int i = 2; i <= n; i++) {
+    if (is_prime(i)) {
+      printf("%d\n", i);
+    }
+  }
 
-    // TODO: validate input and print all primes up to n
+  // TODO: validate input and print all primes up to n
 
-    return 0;
+  return 0;
 }
